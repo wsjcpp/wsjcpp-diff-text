2c6ca53fc3cfe100d556c952dff2f34b0813b6d2
Evgenii Sopov <mrseakg@gmail.com>
2026-07-23 10:11:11 +0700

    Fix format src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,46 +5,47 @@
 #include <wsjcpp_diff_text.h>
 
 int main(int argc, const char* argv[]) {
-    if (argc != 3) {
-        std::cout << "Usage: " << argv[0] << " file1 file2" << std::endl;
-        return -1;
-    }
+  if (argc != 3) {
+    std::cout << "Usage: " << argv[0] << " file1 file2" << std::endl;
+    return -1;
+  }
 
-    std::string TAG = "MAIN";
-    std::string appName = std::string(WSJCPP_APP_NAME);
-    std::string appVersion = std::string(WSJCPP_APP_VERSION);
-    if (!WsjcppCore::dirExists(".logs")) {
-        WsjcppCore::makeDir(".logs");
-    }
-    WsjcppLog::setPrefixLogFile("wsjcpp-diff-text");
-    WsjcppLog::setLogDirectory(".logs");
+  std::string TAG = "MAIN";
+  std::string appName = std::string(WSJCPP_APP_NAME);
+  std::string appVersion = std::string(WSJCPP_APP_VERSION);
+  if (!WsjcppCore::dirExists(".logs")) {
+    WsjcppCore::makeDir(".logs");
+  }
+  WsjcppLog::setPrefixLogFile("wsjcpp-diff-text");
+  WsjcppLog::setLogDirectory(".logs");
 
-    std::string sFileName1(argv[1]);
-    std::string sFileName2(argv[2]);
+  std::string sFileName1(argv[1]);
+  std::string sFileName2(argv[2]);
 
-    if (!WsjcppCore::fileExists(sFileName1)) {
-        WsjcppLog::err(TAG, "file1 not found '" + sFileName1 + "'");
-        return -1;
-    }
+  if (!WsjcppCore::fileExists(sFileName1)) {
+    WsjcppLog::err(TAG, "file1 not found '" + sFileName1 + "'");
+    return -1;
+  }
 
-    if (!WsjcppCore::fileExists(sFileName2)) {
-        WsjcppLog::err(TAG, "file2 not found '" + sFileName2 + "'");
-        return -1;
-    }
+  if (!WsjcppCore::fileExists(sFileName2)) {
+    WsjcppLog::err(TAG, "file2 not found '" + sFileName2 + "'");
+    return -1;
+  }
 
-    std::string sFileContent1;
-    std::string sFileContent2;
-    WsjcppCore::readTextFile(sFileName1, sFileContent1);
-    WsjcppCore::readTextFile(sFileName2, sFileContent2);
+  std::string file_content1;
+  std::string file_content2;
+  WsjcppCore::readTextFile(sFileName1, file_content1);
+  WsjcppCore::readTextFile(sFileName2, file_content2);
 
-    std::vector<wsjcpp::diff_text_row> vResult;
-    wsjcpp::diff_text_compare(sFileContent1, sFileContent2, vResult);
-    for (int i = 0; i < vResult.size(); i++) {
-        std::cout << 
-            vResult[i].getKey() << " => line (" << vResult[i].getNumberOfLine() << "): " 
-            << vResult[i].getLine() << std::endl;
-    }
-    vResult.clear();
-    return 0;
+  std::vector<wsjcpp::diff_text_row> result;
+  wsjcpp::diff_text_compare(file_content1, file_content2, result);
+  for (int i = 0; i < result.size(); i++) {
+    std::cout
+      << result[i].getKey() << " => line (" << result[i].getNumberOfLine() << "): "
+      << result[i].getLine()
+      << std::endl;
+  }
+  result.clear();
+  return 0;
 }
 
